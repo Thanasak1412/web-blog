9db7a5a3a11b45f2963bdb326e3db52d5c36d5e2
Thanasak <Thanasaksrisaeng@gmail.com>
2021-04-28 01:48:42 +0700

    update database, route css

--- app.js
+++ app.js
@@ -2,13 +2,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum repellat delectus esse in optio, voluptatibus vitae. Iusto aspernatur incidunt maiores pariatur laborum ipsam, eum architecto obcaecati est explicabo delectus possimus!";
-const contactContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima earum quo, quibusdam voluptatem tempore quisquam. A consectetur non minus ab quam nihil qui. Atque adipisci quasi voluptatum cumque veritatis natus?";
-const aboutContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae odit est minus nisi, accusamus asperiores magnam, eligendi praesentium neque aliquid nulla itaque alias fugit pariatur repudiandae, illum delectus aut. Quis?";
-let posts = [];
+const homeStartingContent = "Lorem ipsum dolor sit amet, consectet";
+const contactContent = "Lorem ipsum dolor sit";
+const aboutContent = "Lorem ipsum dolor";
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/web-blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const postSchema = mongoose.Schema({
+  title: {
+    type: String,
+    min: 1,
+  },
+  content: {
+    type: String,
+    min: 1,
+  },
+});
+
+const Post = mongoose.model("Post", postSchema);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/public`));
@@ -16,7 +34,13 @@ app.use(express.static(`${__dirname}/public`));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("home", { startContent: homeStartingContent, posts: posts });
+  Post.find({}, (err, posts) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", { startContent: homeStartingContent, posts: posts });
+    }
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -31,25 +55,27 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
-app.get("/posts/:title", (req, res) => {
-  posts.forEach(({title, content}) => {
-    const reqTitle = _.lowerCase(req.params.title);
-
-    if (reqTitle === _.lowerCase(title)) {
-      res.render("post", { title: title, content: content });
-    } else {
-      res.render("post", { title: "404 Not Found", content: "" });
-    }
-  })
-})
-
 app.post("/compose", (req, res) => {
-  const post = {
+  const post = new Post({
     title: req.body.inputTitle,
-    content: req.body.inputContent
-  }
-  posts.push(post);
-  res.redirect("/");
+    content: req.body.inputContent,
+  });
+
+  post.save((err) => {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", (req, res) => {
+  const { postId } = req.params;
+
+  Post.findOne({ _id: postId }, (err, { title, content }) => {
+    if (!err) {
+      res.render("post", { title: title, content: content });
+    }
+  });
 });
 
 app.listen(process.env.PORT || 3000, () => {
